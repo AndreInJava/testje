cae06da94c922204e5f97e9f1698aa9bb556c362
Andre Goossens <andre.goossens@additude.eu>
2020-04-22 20:54:42 +0200

    pasIetsAan

--- src/TestHub.java
+++ src/TestHub.java
@@ -2,4 +2,8 @@ public class TestHub {
     public static void main(String[] args) {
         System.out.println("Test hubje");
     }
+    public void pasIetsAan() {
+        int a = 10;
+        String githubbie =  "Githubbie";
+    }
 }
